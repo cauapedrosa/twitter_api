4fc8565f76d8ffe232d4c7543b9e890a270a7f18
Cauã <cauapedrosa@live.com>
2022-09-05 14:46:09 -0300

    v1

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import tweepy
 
 # Your twitter handle
 user_screen_name = 'username'
-# If tweet doesn't have X likes, delete it.
+# If tweet doesn't have X or more likes, delete it.
 min_likes = 1
 
 
@@ -16,7 +16,8 @@ def main():
     api = tweepy.API(auth)
     # Display user info
     user = api.get_user(screen_name=user_screen_name)._json
-    print('Starting to work on user: ' + user['name'] + ' (@' + user_screen_name + ')\n')
+    print('\nTweet Deleter v1\nStarting to work on user: ')
+    print(user['name'] + ' (@' + user_screen_name + ')\n')
 
     # Gets tweets
     timelines = api.user_timeline(
@@ -24,17 +25,21 @@ def main():
 
     # Iterates over tweets
     for n, status in enumerate(timelines):
-        twt_list_size = len(timelines)
-        tweetid = status._json['id']
-        like_count = status._json['favorite_count']
-        metrics = int(like_count)
-        print(f'\n#{n+1}/{twt_list_size} [ID:{tweetid}] Likes: {like_count}')
-        if metrics < min_likes:
-            print(f'#{n+1}/{twt_list_size} [ID:{tweetid}] Deleting...  - {metrics}')
-            # Uncomment the next command to enable deleting
+        len_twts = len(timelines)
+        # Extracts tweet json
+        tweet = status._json
+        twt_id = tweet['id']
+        twt_metrics = int(tweet['favorite_count'])
+        twt_content = tweet['text']
+        
+        print(f'\n#{n+1}/{len_twts} - ID:{twt_id} Metrics of {twt_metrics}')
+        print(f'Content: "' + twt_content +'"')
+        if twt_metrics < min_likes:
+            print(f'#{n+1}/{len_twts} - ID:{twt_id} will be deleted. Metrics {twt_metrics} is less than {min_likes}')
+            # Uncomment the next command to enable deleting. Leave it commented to perform a test run.
             # api.destroy_status(tweetid)
         else:
-            print(f'#{n+1}/{twt_list_size} [ID:{tweetid}] will be spared. Metrics {metrics} surpass {min_likes}')
+            print(f'#{n+1}/{len_twts} - ID:{twt_id} will be spared. Metrics {twt_metrics} is greater than {min_likes}')
 
 
 if __name__ == "__main__":
